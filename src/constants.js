619ff9171762c559fc40eca0d2d8c58033375f92
Mukul kumar <mukulkumaruk08@gmail>
2026-01-05 22:13:13 +0530

    feat: Add new components and assets for portfolio website
    - Created About, Contact, Education, Footer, Navbar, Skills, and Work components.
    - Implemented email functionality in Contact component using EmailJS.
    - Added responsive design and smooth scrolling for navigation.
    - Included new tech logos and work logos in assets.
    - Established constants for skills and education data.
    - Integrated Tailwind CSS for styling and animations.
    - Set up Vite for project bundling and development.

---
+++ src/constants.js
@@ -0,0 +1,168 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import cLogo from './assets/tech_logo/c.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+
+
+
+// Education Section Logo's
+import uulogo from './assets/education_logo/uulogo.jpg'
+import svmlogo from './assets/education_logo/svmlogo.jpg';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+   
+      { name: 'Redux', logo: reduxLogo },
+    
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+
+    
+     
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+     
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+
+  
+  export const education = [
+  
+    {
+      id: 1,
+      img: uulogo,
+      school: "Uttaranchal University",
+      date: "July 2024 - July 2027",
+      grade: "",
+      desc: "I completed my Bachelor's of Computer applications from Uttaranchal University, Dehreadun. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BCA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+      degree: "Bachelor of Computer Applications - BCA ",
+    },
+    {
+      id: 2,
+      img: svmlogo,
+      school: "SVM Inter College Mayapur , Haridwar",
+      date: "Apr 2023 - March 2024",
+      grade: "58%",
+      desc: "I completed my class 12 education from SVM Inter College , Haridwar, under the NCRT board, where I studied Physics, Chemistry, and Mathematics (PCM).",
+      degree: "NCRT(XII) - PCM ",
+    },
+    {
+      id: 3,
+      img: svmlogo,
+      school: "SVM Inter College Mayapur , Haridwar",
+      date: "Apr 2020 - March 2021",
+      grade: "61%",
+      desc: "I completed my class 10 education from SVM Inter College  Haridwar, under the NCRT board, where I studied Science.",
+      degree: "NCRT(X), Science",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "GitHub Profile Detective",
+      description:
+        "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+      image: githubdetLogo,
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+      github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+      webapp: "https://githubprofiledetective.netlify.app/",
+    },
+  
+    {
+      id: 2,
+      title: "Movie Recommendation App",
+      description:
+        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/codingmastr/Movie-Recommendation-App",
+      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+
+ 
+    {
+      id: 7,
+      title: "Image Search App",
+      description:
+        "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+      image: imagesearchLogo,
+      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+      github: "https://github.com/codingmastr/Image-Search-App",
+      webapp: "https://imagsearch.netlify.app/",
+    },
+    {
+      id: 8,
+      title: "Image Background Remover",
+      description:
+        "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+      image: removebgLogo,
+      tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+      github: "https://github.com/codingmastr/Image-Background-Remover",
+      webapp: "https://removeyourbg.netlify.app/",
+    },
+  ];  
